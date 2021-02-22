48366821558c374db10072c50613ccd135af13fb
omojibola <omojibolaorija@gmail.com>
2021-02-23 00:56:06 +0100

    implemented role authorization

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -8,11 +8,17 @@ const {
   updateProduct,
   deleteProduct,
 } = require('../controllers/productController');
+const { isAuthenticated, authorizeRoles } = require('../middlewares/auth');
 
-router.route('/products').get(getProducts);
+router.route('/products').get(isAuthenticated, getProducts);
 router.route('/product/:id').get(getSingleProduct);
 
-router.route('/admin/product/new').post(newProduct);
-router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct);
+router
+  .route('/admin/product/new')
+  .post(isAuthenticated, authorizeRoles('admin'), newProduct);
+router
+  .route('/admin/product/:id')
+  .put(isAuthenticated, authorizeRoles('admin'), updateProduct)
+  .delete(isAuthenticated, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
